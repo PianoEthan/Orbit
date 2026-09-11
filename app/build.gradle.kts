72a9a71a174bb76acce5f8f070a8b8640158f04a
Star_ZER0 <m@star0.cc>
2026-09-12 01:13:47 +0800

    feat: 新增小米手表5系列震动支持
    使用miwearhaptics兼容插件调用小米手表5系统中com.xiaomi.miwear.input.WearHapticFeedbackConstants的对应方法，解决了小米手表5系列使用表冠浏览页面时没有震动的问题
    
    Signed-off-by: Star_ZER0 <m@star0.cc>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("kotlin-parcelize")
+    id("cc.star0.wear.lib.miwearhaptics")
 }
 
 val releaseKeystorePath = providers.environmentVariable("ORBIT_KEYSTORE_FILE").orNull
